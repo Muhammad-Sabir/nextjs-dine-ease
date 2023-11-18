00b9855658349b32de611ded78a057cac7d85a81
Muhammad Sabir <sabir.tabi1122@gmail.com>
2023-11-18 13:05:07 +0500

    Update navbar style

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -5,14 +5,14 @@ import logo from "$/logo.png";
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between px-4">
+    <nav className="flex items-center justify-between px-7">
       <Image src={logo} alt="Img" width={140} height={70} />
 
       <Link
         href="/login"
-        className="border-primary text-primary hover:bg-primary hover:text-tertiary rounded-lg border-2 border-solid px-2 py-1 font-medium"
+        className="border-primary text-primary hover:bg-primary hover:text-tertiary rounded-sm border-2 border-solid px-2 py-1"
       >
-        Login
+        LOGIN
       </Link>
     </nav>
   );
