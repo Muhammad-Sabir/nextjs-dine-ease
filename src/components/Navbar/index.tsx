cbd9b6b3b3f95a05a7f0511c33323e9c85bc4429
Muhammad Sabir <sabir.tabi1122@gmail.com>
2023-11-18 13:35:32 +0500

    Update navbar for small devices

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { FaCircleUser } from "react-icons/fa6";
 
 import logo from "$/logo.png";
 
@@ -8,12 +9,25 @@ export default function Navbar() {
     <nav className="bg-tertiary sticky top-0 flex items-center justify-between px-7">
       <Image src={logo} alt="Img" width={140} height={70} />
 
-      <Link
-        href="/login"
-        className="border-primary text-primary hover:bg-primary hover:text-tertiary rounded-sm border-2 border-solid px-2 py-1"
-      >
-        LOGIN
-      </Link>
+      <div className="flex gap-4">
+        <Link
+          href="/signup"
+          className="border-primary text-primary hover:bg-primary hover:text-tertiary hidden rounded-sm border-2 border-solid px-2 py-1 md:flex"
+        >
+          Sign Up
+        </Link>
+
+        <Link
+          href="/login"
+          className="bg-primary text-tertiary border-primary hover:bg-tertiary hover:text-primary hidden rounded-sm border-2 border-solid px-2 py-1 md:flex"
+        >
+          <FaCircleUser className="mr-2" /> Login
+        </Link>
+
+        <Link href="/login">
+          <FaCircleUser className="text-primary text-2xl md:hidden" />
+        </Link>
+      </div>
     </nav>
   );
 }
